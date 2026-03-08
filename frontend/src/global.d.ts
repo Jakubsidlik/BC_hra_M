21e0b8849df2af47e85ca2b3fd4500ee4ca18603
Jakubsidlik <jakubsidlik29@gmail.com>
2026-03-08 15:42:54 +0100

    config_change

---
+++ frontend/src/global.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+
+// This file provides a catch-all JSX.IntrinsicElements declaration
+// to satisfy the language server when React types are not picked up properly.
+// It should be safe since actual element typings come from React at runtime.
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
+}
